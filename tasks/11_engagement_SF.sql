f2126941aa65d9577d78ea2e71e970c44a350bdd
Tanya Abramovskaia <tanya.abramovskaya@gmail.com>
2025-10-07 14:37:35 +0200

    Create 11_engagement_SF.sql
    Added solution for SF analysis

---
+++ tasks/11_engagement_SF.sql
@@ -0,0 +1,37 @@
+/*
+How do DAU/MAU and the Sticky Factor change by month? Is there seasonality?
+*/
+
+select 
+	to_char(monthly_active_users.session_month, 'yyyy-mm') as month,
+	round(avg_daily_active_users.avg_DAU / monthly_active_users.MAU, 4) as SF
+from (
+	select 
+		date_trunc('month', session_date) as session_month, 
+		count(distinct user_id) as MAU
+	from user_sessions us
+	group by date_trunc('month', session_date)
+	) as monthly_active_users
+inner join (
+	select 
+		date_trunc('month', daily.session_date) as session_month,
+		avg(daily.DAU) as avg_DAU
+	from (
+	select session_date, count(distinct user_id) as DAU
+	from user_sessions us
+	group by session_date
+	) as daily
+	group by date_trunc('month', daily.session_date)
+	) as avg_daily_active_users
+on monthly_active_users.session_month = avg_daily_active_users.session_month;
+
+/*
+Result:
+month  |sf    |
+-------+------+
+2024-01|0.4056|
+2024-02|0.3543|
+2024-03|0.2948|
+
+I don’t observe any significant difference in SF across the months.
+*/
