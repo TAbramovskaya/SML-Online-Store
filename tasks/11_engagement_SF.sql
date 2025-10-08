d278074cda93ee47561634eb8e1c5090e9d9104b
Tanya Abramovskaia <tanya.abramovskaya@gmail.com>
2025-10-08 09:26:35 +0200

    Corrected approach to calculating SF
    The updated version uses the calendar month and its duration to average the DAU.

--- tasks/11_engagement_SF.sql
+++ tasks/11_engagement_SF.sql
@@ -4,7 +4,9 @@ How do DAU/MAU and the Sticky Factor change by month? Is there seasonality?
 
 select 
 	to_char(monthly_active_users.session_month, 'yyyy-mm') as month,
-	round(avg_daily_active_users.avg_DAU / monthly_active_users.MAU, 4) as SF
+	round(sum_daily_active_users.sum_DAU / 
+		(monthly_active_users.MAU * 
+		extract('day' from (sum_daily_active_users.session_month + interval '1 month - 1 day'))), 4) as SF
 from (
 	select 
 		date_trunc('month', session_date) as session_month, 
@@ -15,21 +17,21 @@ from (
 inner join (
 	select 
 		date_trunc('month', daily.session_date) as session_month,
-		avg(daily.DAU) as avg_DAU
+		sum(daily.DAU) as sum_DAU
 	from (
 	select session_date, count(distinct user_id) as DAU
 	from user_sessions us
 	group by session_date
 	) as daily
 	group by date_trunc('month', daily.session_date)
-	) as avg_daily_active_users
-on monthly_active_users.session_month = avg_daily_active_users.session_month;
+	) as sum_daily_active_users
+on monthly_active_users.session_month = sum_daily_active_users.session_month 
 
 /*
 Result:
 month  |sf    |
 -------+------+
-2024-01|0.4056|
+2024-01|0.3532|
 2024-02|0.3543|
 2024-03|0.2948|
 
