fd86335ecc253a2a4b6ca9a45ed67f8abc759418
Tanya Abramovskaia <tanya.abramovskaya@gmail.com>
2025-10-16 23:44:08 +0200

    Error correction and query optimization

--- tasks/02_acquisition_LTV_30d.sql
+++ tasks/02_acquisition_LTV_30d.sql
@@ -5,41 +5,30 @@ Compare 30-day customer value across acquisition channels.
 
 with first_30_days as (
 	select 
-		retention_from.user_id,
-		retention_from.channel,
-		case 
-			when orders.order_timestamp - retention_from.day_0 <= 30*interval '1 day' then orders.total_amount 
-			else null 
-		end as amount
-	from (
-		select 
-			u.user_id, 
-			u.acquisition_channel as channel,
-			min(us.session_start) as day_0
-		from users u 
-		left join user_sessions us 
-		on u.user_id = us.user_id 
-		group by u.user_id
-		) as retention_from
-	left join orders
-	on retention_from.user_id = orders.user_id
-	) 
+		users.user_id,
+        users.acquisition_channel as channel,
+        orders.total_amount as amount
+    from users
+    inner join orders
+    on users.user_id = orders.user_id and
+	    orders.order_timestamp - users.registration_date <= 30 * INTERVAL '1 day' and
+		orders.status = 'completed'
+)
 select 
-	first_30_days.channel as channel, 
-	round(sum(first_30_days.amount) / count(distinct first_30_days.user_id), 4) as LTV_30d
+	first_30_days.channel as channel,
+    round(sum(first_30_days.amount) / count(DISTINCT first_30_days.user_id), 4) as LTV_30d
 from first_30_days
-where amount is not null
-group by first_30_days.channel 
+group by first_30_days.channel
 order by LTV_30d desc;
 
 /*
 Result:
     channel    |ltv_30d  |
     -----------+---------+
-    email      |2483.7113|
-    organic    |1989.1242|
-    social     |1917.6690|
-    paid_search|1669.8075|
+    email      |2458.7125|
+    organic    |1830.0475|
+    social     |1816.7720|
+    paid_search|1752.6100|
 
 Although users acquired via email show the highest 30-day LTV, given the dataset size, 
 we can conclude that the 30-day LTV across different acquisition channels 
